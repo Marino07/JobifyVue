55324a13a31fa7d8e83e2b713adf147d86d45d5c
Marino Pušić <marino.pusic.5@gmail.com>
2024-10-04 20:35:06 +0200

    route setup

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <script setup>
-import JobListnings from './components/JobListnings.vue';
 import Navbar from './components/Navbar.vue';
-import Hero from './components/Hero.vue';
-import HomeCards from './components/HomeCards.vue';
+import { RouterView } from 'vue-router';
+
 
 </script>
 
 
 <template>
   <Navbar></Navbar>
-  <Hero title="Become Laravel dev" subtitle="Dont worry you got this"></Hero>
-  <HomeCards></HomeCards>
-  <JobListnings :limit = 3 :showButton="true"></JobListnings>
+  <RouterView/>
  
 </template>
 
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import JobListnings from '@/components/JobListnings.vue';
+import Hero from '@/components/Hero.vue';
+import HomeCards from '@/components/HomeCards.vue';
+</script>
+
+<template>
+    <Hero title="Become Laravel dev" subtitle="Dont worry you got this"></Hero>
+    <HomeCards></HomeCards>
+    <JobListnings :limit = 3 :showButton="true"></JobListnings>
+</template>
