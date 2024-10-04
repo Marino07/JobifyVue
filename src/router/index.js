f73e5e2ed698f384bfa0ab206fff01d3c3d9adf1
Marino Pušić <marino.pusic.5@gmail.com>
2024-10-04 22:01:59 +0200

    pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import {createRouter, createWebHistory} from 'vue-router';
 import HomeView from '@/views/HomeView.vue';
 import JobsView from '@/views/JobsView.vue';
+import NotFound from '@/views/NotFound.vue';
+import JobView from '@/views/JobView.vue';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -15,6 +17,17 @@ const router = createRouter({
             name: 'jobs',
             component: JobsView
         },
+        {
+            path: '/jobs/:id',
+            name: 'Job',
+            component: JobView
+
+        },
+        {
+            path: '/:catchAll(.*)',
+            name: 'NotFound',
+            component: NotFound
+        }
         
     ]
 });
